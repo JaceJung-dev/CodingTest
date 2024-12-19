df2937e9bc7794b371ddd11b50895ad86aedce52
Jace Jung <jacejung.dev@gmail.com>
2024-12-19 09:41:30 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,9 +1,2 @@
 def solution(a, b):
-    answer = 0
-    res1 = int(f'{a}{b}')
-    res2 = int(f'{b}{a}')
-    if res1 >= res2:
-        answer = res1
-    else:
-        answer = res2
-    return answer
+    return max(int(f"{a}{b}"), int(f"{b}{a}"))
