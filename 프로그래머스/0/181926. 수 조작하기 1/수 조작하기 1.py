6c239a086119c249dc65f11f0c13b53749d71abb
Jace Jung <jacejung.dev@gmail.com>
2024-12-19 09:12:47 +0900

    [level 0] Title: 수 조작하기 1, Time: 9.47 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -0,0 +1,14 @@
+def solution(n, control):
+    answer = n
+    for char in control:
+        if char == 'w':
+            answer += 1
+        elif char == 's':
+            answer -= 1
+        elif char == 'd':
+            answer += 10
+        elif char == 'a':
+            answer -= 10
+        else:
+            pass
+    return answer
