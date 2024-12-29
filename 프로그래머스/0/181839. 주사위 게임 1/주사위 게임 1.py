ecf96acd50a5292986003e1d83ea60b5a0eb128c
Jiwoong Jung <jacejung.dev@gmail.com>
2024-12-30 08:55:48 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.py
@@ -0,0 +1,9 @@
+def solution(a, b):
+    answer = 0
+    if a % 2 == 0 and b % 2 == 0:
+        answer = abs(a - b)
+    elif a * b % 2 == 0:
+        answer = 2 * (a + b)
+    else:
+        answer = a ** 2 + b ** 2 
+    return answer
