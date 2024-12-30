eed0f5ab87722bb06caf9905006a873a2c819f33
Jiwoong Jung <jacejung.dev@gmail.com>
2024-12-30 09:07:29 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.py
@@ -0,0 +1,9 @@
+def solution(str_list, ex):
+    answer = ''
+    for char in str_list:
+        if ex not in char:
+            answer += char
+        else:
+            pass
+            
+    return answer
