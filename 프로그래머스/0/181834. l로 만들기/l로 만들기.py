68eec0d316f72e4fd5327890c3395ee3901341c4
Jiwoong Jung <jacejung.dev@gmail.com>
2025-01-02 10:30:08 +0900

    [level 0] Title: l로 만들기, Time: 11.39 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -0,0 +1,9 @@
+def solution(myString):
+    answer = ''
+    for char in myString:
+        if ord(char) < 108:
+            answer += 'l'
+        else:
+            answer += char
+            
+    return answer
