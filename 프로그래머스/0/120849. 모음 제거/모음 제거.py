b4700e1c050bc5f8310018b0ed01f64ccc625048
Jiwoong Jung <jacejung.dev@gmail.com>
2025-01-17 09:11:44 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,7 @@
+def solution(my_string):
+    answer = ''
+    vowels = ['a', 'e', 'i' , 'o', 'u']
+    for char in my_string:
+        if char not in vowels:
+            answer += char
+    return answer
