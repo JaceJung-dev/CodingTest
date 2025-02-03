04c675b201dc5549e7da8e173a854b7ee6d8a0e5
Jiwoong Jung <jacejung.dev@gmail.com>
2025-02-03 09:15:46 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 13.11 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.py
@@ -0,0 +1,8 @@
+def solution(arr, queries):
+    answer = []
+    for query in queries:
+        for i in range(query[0],query[-1]+1):
+            arr[i] += 1
+            
+    answer = arr
+    return answer
