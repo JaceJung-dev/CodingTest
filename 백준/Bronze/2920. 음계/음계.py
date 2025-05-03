65b0be42c04e0ef37817a413bc3b852899c21538
Jiwoong Jung <jacejung.dev@gmail.com>
2025-05-03 13:14:21 +0900

    [Bronze II] Title: 음계, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,8 @@
+check_list = list(map(int, input().split()))
+
+if check_list == [1, 2, 3, 4, 5, 6, 7, 8]:
+    print("ascending")
+elif check_list == [8, 7, 6, 5, 4, 3, 2, 1]:
+    print("descending")
+else:
+    print("mixed")
