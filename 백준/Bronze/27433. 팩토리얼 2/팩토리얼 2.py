8aa276a5885e6ee7481aeea7cff21b15c198195a
Jiwoong Jung <jacejung.dev@gmail.com>
2025-06-04 08:35:37 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.py
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.py
@@ -1,8 +1,7 @@
 N = int(input())
 
-def factorial(n):
-    if n <= 1:
-        return 1
-    return n * factorial(n-1)
-
-print(factorial(N))
+total = 1
+for num in range(2, N + 1):
+    total *= num
+    
+print(total)
