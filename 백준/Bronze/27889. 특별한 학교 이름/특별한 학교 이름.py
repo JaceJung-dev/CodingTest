fb6f23d841271521c6afa67d368f7dca805c891a
Jiwoong Jung <jacejung.dev@gmail.com>
2025-06-12 10:24:17 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,10 @@
+GEC_SCHOOL = {
+    "NLCS": "North London Collegiate School",
+    "BHA": "Branksome Hall Asia",
+    "KIS": "Korea International School",
+    "SJA": "St. Johnsbury Academy",
+}
+
+user_input = input()
+
+print(GEC_SCHOOL[user_input])
