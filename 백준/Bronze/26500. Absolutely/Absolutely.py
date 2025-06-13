e26edefa880221c234a1bfc92b83fac9d02219e9
Jiwoong Jung <jacejung.dev@gmail.com>
2025-06-13 10:27:30 +0900

    [Bronze IV] Title: Absolutely, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/26500. Absolutely/Absolutely.py
+++ 백준/Bronze/26500. Absolutely/Absolutely.py
@@ -2,8 +2,6 @@ N = int(input())
 
 for _ in range(N):
     a, b = map(float, input().split())
-    abs_distance = a - b
-    if abs_distance < 0:
-        abs_distance *= -1
+    abs_distance = abs(a - b)
     
     print(round(abs_distance, 1))
