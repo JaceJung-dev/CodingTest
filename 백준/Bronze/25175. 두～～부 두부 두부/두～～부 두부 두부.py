c2953607d391541579e01dfb5c37dfc86b2767cb
JaceJung-dev <jacejung.dev@gmail.com>
2025-06-15 12:24:16 +0900

    [Bronze III] Title: 두~~부 두부 두부, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/25175. 두～～부 두부 두부/두～～부 두부 두부.py
+++ 백준/Bronze/25175. 두～～부 두부 두부/두～～부 두부 두부.py
@@ -1,9 +1,14 @@
 # 1. 전체 인원, 현재 기준점 M, 이동하는 값 K
 # 2. 순환형이니까 모듈러 생각해보기
+# 3. K가 음수이면 양수가 될 때까지 N 더해주기
 
 N, M, K = map(int, input().split())
-next_person = (M + K - 3) % N
-if next_person == 0:
-    print(N)
-else:
-    print(next_person)
+
+while K < 0:
+    K += N
+    
+next_person = M + ((K - 3) % N)
+if next_person > N:
+    next_person %= N
+    
+print(next_person)
