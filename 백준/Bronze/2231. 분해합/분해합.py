a43abbbef3abafa67f996fadff12cd99a9db6e65
Jiwoong Jung <jacejung.dev@gmail.com>
2025-06-24 08:23:57 +0900

    [Bronze II] Title: 분해합, Time: 1724 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+constructor = 0
+for num in range(1, N + 1):
+    each_sum = 0
+    for each_num in str(num):
+        each_sum += int(each_num)
+        total = num + each_sum
+    
+    if total == N:
+        constructor = num
+        break
+        
+print(constructor)
+
+      
