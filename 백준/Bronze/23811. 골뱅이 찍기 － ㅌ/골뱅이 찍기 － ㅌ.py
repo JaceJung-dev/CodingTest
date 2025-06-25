ea1bc451c6651d7f9d20d7ad5e9efa306c531b73
Jiwoong Jung <jacejung.dev@gmail.com>
2025-06-25 09:34:27 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅌ, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/23811. 골뱅이 찍기 － ㅌ/골뱅이 찍기 － ㅌ.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+base = "@" * N
+
+for _ in range(N):
+    print(base * 5)
+for _ in range(N):
+    print(base)
+for _ in range(N):
+    print(base * 5)
+for _ in range(N):
+    print(base)
+for _ in range(N):
+    print(base * 5)
