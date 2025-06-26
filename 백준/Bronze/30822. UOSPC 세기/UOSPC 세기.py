b5212d84cc3d59e2ec2c6f4e4246be9bb9183606
Jiwoong Jung <jacejung.dev@gmail.com>
2025-06-27 08:25:04 +0900

    [Bronze III] Title: UOSPC 세기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/30822. UOSPC 세기/UOSPC 세기.py
+++ 백준/Bronze/30822. UOSPC 세기/UOSPC 세기.py
@@ -1,23 +1,6 @@
 n = int(input())
 word = input()
 
-letter_count = {
-    "u": 0,
-    "o": 0,
-    "s": 0,
-    "p": 0,
-    "c": 0,
-}
+letter_count = [word.count(i) for i in "usopc"]
 
-for letter in word:
-    for k, v in letter_count.items():
-        if k == letter:
-            letter_count[letter] += 1
-
-min_count = float("inf")
-for k, v in letter_count.items():
-    if v < min_count:
-        min_count = v
-        
-print(min_count)
-   
+print(min(letter_count))
