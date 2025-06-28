d97b1f2c677a69127889372ff4cc0047714208c5
Jiwoong Jung <jacejung.dev@gmail.com>
2025-06-28 13:48:52 +0900

    [Bronze II] Title: 3대 측정, Time: 1008 ms, Memory: 75796 KB -BaekjoonHub

--- 백준/Bronze/20299. 3대 측정/3대 측정.py
+++ 백준/Bronze/20299. 3대 측정/3대 측정.py
@@ -2,28 +2,15 @@ import sys
 input = sys.stdin.readline
 
 N, K, L = map(int, input().split())
+vip_members = []
 vip_count = 0
-vip_member = []
 
 for _ in range(N):
     team = list(map(int, input().split()))
     
-    if sum(team) < K:
-        continue
-    
-    is_disqulified = False
-    for person in team:
-        if person < L:
-            is_disqulified = True
-    
-    if is_disqulified:
-        continue
-    
-    vip_count += 1
-    vip_member += team
-    
+    if sum(team) >= K and all(member >= L for member in team):
+        vip_count += 1
+        vip_members.extend(team)
+
 print(vip_count)
-for person in vip_member:
-    print(person, end=" ")
-    
-    
+print(*vip_members)
