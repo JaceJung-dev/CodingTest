e0ea1368b13b58ddd071bd747d45b332f6f42ea9
JaceJung-dev <jacejung.dev@gmail.com>
2025-07-02 10:03:41 +0900

    [Bronze III] Title: 주사위 게임, Time: 60 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Bronze/2476. 주사위 게임/주사위 게임.py
+++ 백준/Bronze/2476. 주사위 게임/주사위 게임.py
@@ -1,22 +1,24 @@
 import sys
+from collections import Counter
+
 input = sys.stdin.readline
 
 N = int(input())
-highest_price = 0
+highest_prize = 0
 
 for _ in range(N):
     dices = list(map(int, input().split()))
-    dices.sort()
-    
-    dices_set = set(dices)
-    if len(dices_set) == 1:
-        price = 10000 + 1000 * dices[0]
-    elif len(dices_set) == 2:
-        price = 1000 + 100 * dices[1]
-    elif len(dices_set) == 3:
-        price = 100 * dices[2]
-        
-    if highest_price < price:
-        highest_price = price
-        
-print(highest_price)
+    count = Counter(dices)
+
+    if 3 in count.values():
+        dice = [k for k, v in count.items() if v == 3][0]
+        prize = 10000 + dice * 1000
+    elif 2 in count.values():
+        dice = [k for k, v in count.items() if v == 2][0]
+        prize = 1000 + dice * 100
+    else:
+        prize = max(dices) * 100
+
+    highest_prize = max(highest_prize, prize)
+
+print(highest_prize)
