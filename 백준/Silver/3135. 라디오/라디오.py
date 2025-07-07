4ae70d55717493145792f59b0e2c75fd604db008
Jiwoong Jung <jacejung.dev@gmail.com>
2025-07-08 08:31:11 +0900

    [Silver V] Title: 라디오, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/3135. 라디오/라디오.py
+++ 백준/Silver/3135. 라디오/라디오.py
@@ -3,14 +3,11 @@ input = sys.stdin.readline
 
 cur_frequency, next_frequency = map(int, input().split())
 N = int(input())
-favorite = [int(input()) for _ in range(N)]
 
-cur_gap = abs(next_frequency - cur_frequency)
+min_gap = abs(next_frequency - cur_frequency)
 
-favorite_to_next_gap = [abs(next_frequency - frequency) for frequency in favorite]
-min_gap = min(favorite_to_next_gap) + 1
-
-if min_gap < cur_gap:
-    print(min_gap)
-else:
-    print(cur_gap)
+for _ in range(N):
+    frequency = int(input())
+    min_gap = min(abs(next_frequency - frequency) + 1, min_gap)
+    
+print(min_gap)
