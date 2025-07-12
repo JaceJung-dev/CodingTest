bb20c4316b376cb15e60a280d9949d82ac443a60
Jiwoong Jung <jacejung.dev@gmail.com>
2025-07-12 20:52:29 +0900

    [Bronze I] Title: DRM Messages, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/15087. DRM Messages/DRM Messages.py
+++ 백준/Bronze/15087. DRM Messages/DRM Messages.py
@@ -1,37 +1,23 @@
 import sys
 input = sys.stdin.readline
 
-def get_rotate_num(word):
-    rotate_num = 0
-    for char in word:
-        char_value = ord(char) - ord("A")
-        rotate_num += char_value
-    return rotate_num
+def rotation_value(word):
+    return sum(ord(char) - ord("A") for char in word)
 
-def get_rotated_word(word, rotate_num):
-    rotated_word = ""
-    for char in word:
-        forward_moved = (ord(char) - ord("A") + rotate_num) % 26 + ord("A")
-        rotated_word += chr(forward_moved)
-    return rotated_word
+def rotate(word, k):
+    return "".join(chr((ord(char) - ord("A") + k) % 26 + ord("A")) for char in word) 
 
-def get_merged_word(front, back):
-    merged_word = ""
-    for i in range(len(front)):
-        rotate_num = ord(back[i]) - ord("A")
-        forward_moved = (ord(front[i]) - ord("A") + rotate_num) % 26 + ord("A")
-        merged_word += chr(forward_moved)
-    return merged_word
+def merge(a, b):
+    return ''.join(
+        chr(((ord(x) - ord("A")) + (ord(y) - ord("A"))) % 26 + ord("A")) for x, y in zip(a, b)
+    )
     
     
 N = input().strip()
 half = len(N) // 2
 front, back = N[:half], N[half:]
 
-front_rotate_num = get_rotate_num(front)
-back_rotate_num = get_rotate_num(back)
-  
-front_rotated_word = get_rotated_word(front, front_rotate_num)
-back_rotated_word = get_rotated_word(back, back_rotate_num)
+front_rot = rotate(front, rotation_value(front))
+back_rot = rotate(back, rotation_value(back))
 
-print(get_merged_word(front_rotated_word, back_rotated_word))
+print(merge(front_rot, back_rot))
