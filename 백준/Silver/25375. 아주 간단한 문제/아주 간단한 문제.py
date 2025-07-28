3bafc6e872a185ff3b4a243672aa04808e75e456
Jiwoong Jung <jacejung.dev@gmail.com>
2025-07-28 10:08:19 +0900

    [Silver V] Title: 아주 간단한 문제, Time: 184 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/25375. 아주 간단한 문제/아주 간단한 문제.py
+++ 백준/Silver/25375. 아주 간단한 문제/아주 간단한 문제.py
@@ -1,28 +1,12 @@
 import sys
 input = sys.stdin.readline
 
-def is_coprime(x, y):
-    for i in range(2, min(x, y)):
-        if x % i and y % i == 0:
-            return False
-    return True
 
 N = int(input())
 for _ in range(N):
     a, b = map(int,input().split())
     
-    if b % a != 0:
-        print(0)
-        continue
-        
+    if b % a == 0 and b > a:
+        print(1)
     else:
-        num = b // a
-        for i in range(num // 2):
-            x, y = i, num - i
-            
-            if is_coprime(x, y):
-                print(1)
-                break
-        else:
-            print(0)
-                       
+        print(0)
