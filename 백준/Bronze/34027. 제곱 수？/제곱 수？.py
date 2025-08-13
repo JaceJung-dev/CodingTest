4f0493426bd1649e27218d53a7f8e85cd68cd2bf
Jiwoong Jung <jacejung.dev@gmail.com>
2025-08-13 10:04:57 +0900

    [Bronze III] Title: 제곱 수?, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/34027. 제곱 수？/제곱 수？.py
+++ 백준/Bronze/34027. 제곱 수？/제곱 수？.py
@@ -1,11 +1,11 @@
 import sys
+
 input = sys.stdin.readline
 
 T = int(input())
 for _ in range(T):
     N = int(input())
-    sqrt_N = N ** 0.5
-    if sqrt_N.is_integer():
+    if int(N ** 0.5) ** 2 == N:
         print(1)
     else:
         print(0)
