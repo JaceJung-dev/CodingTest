5a88a8f9f02193d2e37e204be6051c6463481d72
Jiwoong Jung <jacejung.dev@gmail.com>
2025-08-15 08:53:22 +0900

    [Silver V] Title: Best Grass, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/6186. Best Grass/Best Grass.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+R, C = map(int, input().split())
+pasture = [input().rstrip() for _ in range(R)]
+
+count = 0
+clump = [0] * C
+for i in range(R):
+    row = pasture[i]
+    prev = "."
+    for j in range(len(row)):
+        if prev == "#" and row[j] == "#":
+            clump[j] = 1
+            continue
+            
+        if row[j] == "#" and clump[j] == 0:
+            clump[j] = 1
+            count += 1
+        elif row[j] == "." and clump[j] == 1:
+            clump[j] = 0
+        
+        prev = row[j]
+        
+print(count)
+        
