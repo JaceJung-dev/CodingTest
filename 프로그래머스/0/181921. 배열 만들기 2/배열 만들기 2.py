23bec3abace8879fabe628c4612b2f8d252e72b2
Jiwoong Jung <jacejung.dev@gmail.com>
2025-08-19 22:51:57 +0900

    [level 0] Title: 배열 만들기 2, Time: 712.19 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,17 +1,11 @@
 def solution(l, r):
     answer = []
-    except_num = ("1", "2", "3", "4", "6", "7", "8", "9")
+    
     for num in range(l, r + 1):
-        has_exception = False
-        for n in str(num):
-            if n in except_num:
-                has_exception = True
-                break
-            
-        if not has_exception:
+        if not set(str(num)) - set(["0", "5"]):
             answer.append(num)
-        
+            
     if not answer:
         answer.append(-1)
-            
-    return answer
+        
+    return  answer
