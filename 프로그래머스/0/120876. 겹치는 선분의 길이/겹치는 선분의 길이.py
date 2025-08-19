3939beda5d4f84bf99056c15c6d664550cf5bb8f
Jiwoong Jung <jacejung.dev@gmail.com>
2025-08-19 20:02:01 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.08 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -1,14 +1,6 @@
-from collections import Counter
 def solution(lines):
-    answer = 0
-    x_line = []
-    for line in lines:
-        for x in range(line[0], line[1]):
-            x_line.append(x)
-    
-    count = Counter(x_line)
-    for v in count.values():
-        if v >= 2:
-            answer += 1
-            
+    answer = 0  
+    sets = [set(range(line[0], line[1])) for line in lines]
+
+    answer = len(sets[0] & sets[1] | sets[0] & sets[2] | sets[1] & sets[2])
     return answer
