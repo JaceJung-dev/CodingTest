0016e1f7840dcab90de31a5e5ba9ce5c3e89be39
Jiwoong Jung <jacejung.dev@gmail.com>
2025-08-20 10:06:50 +0900

    [Bronze I] Title: ISBN, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/14626. ISBN/ISBN.py
+++ 백준/Bronze/14626. ISBN/ISBN.py
@@ -4,24 +4,20 @@ input = sys.stdin.readline
 isbn = input().rstrip()
 
 total = 0
-unknown_idx = None
-unknown = -1
+
 for i, v in enumerate(isbn[:-1]):
     if v == "*":
-        unknown_idx = i
+        if i % 2 == 0:
+            weight = 1
+        else:
+            weight = 3
         continue
         
     if i % 2 == 0:
         total += int(v)
     else:
         total += 3 * int(v)
-        
-m = int(isbn[-1])
-r = (10 - (total + m) % 10) % 10
 
-if unknown_idx % 2 == 0:
-    unknown = r
-else:
-    unknown = (r * 7) % 10
-    
-print(unknown)
+for i in range(10):
+    if (total + int(isbn[-1]) + weight * i) % 10 == 0:
+        print(i)
