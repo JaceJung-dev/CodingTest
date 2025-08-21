f5af5ee548d483412b3f466b6b4a9430c0b5e35e
Jiwoong Jung <jacejung.dev@gmail.com>
2025-08-21 09:16:18 +0900

    [Bronze I] Title: 동전 게임, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2684. 동전 게임/동전 게임.py
+++ 백준/Bronze/2684. 동전 게임/동전 게임.py
@@ -1,18 +1,15 @@
 import sys
-from collections import Counter
 input = sys.stdin.readline
 
 P = int(input())
+ref = ["TTT", "TTH", "THT", "THH", "HTT", "HTH", "HHT", "HHH"]
 
 for _ in range(P):
-    cases = {"TTT": 0, "TTH": 0, "THT": 0, "THH": 0, 
-             "HTT": 0, "HTH": 0, "HHT": 0, "HHH": 0}
-    
     coin_toss = input().rstrip()
-    for i in range(len(coin_toss) - 2):
-        case = coin_toss[i:i+3]
-        cases[case] += 1
-        
-    answer_list = list(cases.values())
-    print(*answer_list)
+    count = [0] * 8
     
+    for i in range(len(coin_toss) - 2):
+        case = coin_toss[i: i + 3]
+        count[ref.index(case)] += 1
+        
+    print(*count)
