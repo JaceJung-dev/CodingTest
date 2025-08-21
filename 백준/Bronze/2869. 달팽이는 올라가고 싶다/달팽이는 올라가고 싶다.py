dea55ce5ae0bed22eb3f564ca14786ba6f0ceedc
JaceJung-dev <jacejung.dev@gmail.com>
2025-08-21 23:52:40 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 3r62 ms, Memory: 32412 KB -Manual

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -1,10 +1,7 @@
 import sys
-import math
+
 input = sys.stdin.readline
 
 A, B, V = map(int, input().split())
-gap = A - B
 
-days = 1 + math.ceil((V - A) / gap)
-    
-print(days)
+print((V - B + (A - B) - 1) // (A - B))
