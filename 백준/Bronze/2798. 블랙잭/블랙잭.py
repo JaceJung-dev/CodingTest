f918776387bb6886a591f938e79f75c36367765a
Jiwoong Jung <jacejung.dev@gmail.com>
2025-08-21 10:22:49 +0900

    [Bronze II] Title: 블랙잭, Time: 80 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+cards = list(map(int, input().split()))
+
+mini = float("inf")
+for i in range(N):
+    for j in range(i + 1, N):
+        for k in range(j + 1, N):
+            total = cards[i] + cards[j] + cards[k]
+            diff = M - total
+
+            if diff >= 0 and diff <= mini:
+                mini = diff
+
+
+                
+print(M - mini)
