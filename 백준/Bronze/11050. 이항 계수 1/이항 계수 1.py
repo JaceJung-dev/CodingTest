a54378631e1185905f454000a7a4436fe73b69a0
Jiwoong Jung <jacejung.dev@gmail.com>
2025-08-23 10:52:29 +0900

    [Bronze I] Title: 이항 계수 1, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+def factorial(n):
+    num = 1
+    for i in range(1, n + 1):
+        num *= i
+        
+    return num
+        
+
+N, K = map(int, input().split())
+
+print(factorial(N) // (factorial(N - K) * factorial(K)))
