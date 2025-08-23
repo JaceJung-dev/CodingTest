56ed81cb3d9dc70866fa51e021d9f1b791cde2af
Jiwoong Jung <jacejung.dev@gmail.com>
2025-08-23 10:56:33 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,8 +1,5 @@
 def solution(n):
     answer = ''
-    for i in range(n):
-        if i % 2 == 0:
-            answer += "수"
-        else:
-            answer += "박"
+    char = "수박" * n
+    answer = char[:n]
     return answer
