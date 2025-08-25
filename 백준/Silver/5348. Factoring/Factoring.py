0ac1fd899b2928a660fe15cee1f9f7b0b1d946ca
Jiwoong Jung <jacejung.dev@gmail.com>
2025-08-25 11:23:09 +0900

    [Silver III] Title: Factoring, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/5348. Factoring/Factoring.py
+++ 백준/Silver/5348. Factoring/Factoring.py
@@ -1,26 +1,40 @@
 import sys
 input = sys.stdin.readline
 
-def prime_factorization(n):
-    prime = []
-    for i in range(2, n + 1):
-        while True:
-            if n % i == 0:
-                prime.append(i)
-                n //= i
-            else:
-                break
-                
-    return prime
-            
+def get_prime_list(n):
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False
+    for i in range(2, int(n ** 0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, n + 1, i):
+                is_prime[j] = False
+    
+    return [i for i, v in enumerate(is_prime) if v]
+
+def factorize(num, prime_list):
+    factors = []
+    for p in prime_list:
+        if p ** 2 > num:
+            break
+        
+        while num % p ==0:
+            factors.append(p)
+            num //= p
+    if num > 1:
+        factors.append(num)
+    
+    return factors
+
 
 N = int(input())
-for _ in range(N):
-    num = int(input())
-    prime = prime_factorization(num)
-    
-    if len(prime) == 1:
+num_list = [int(input()) for _ in range(N)]
+
+primes = get_prime_list(46340) # (2^31 - 1) ** 0.5
+
+for num in num_list:
+    factors = factorize(num, primes)
+    if len(factors) == 1 and factors[0] == num:
         print(f"{num}: prime")
     else:
-        print(f"{num}:", end=" ")
-        print(*prime)
+        print(f"{num}: {' '.join(map(str, factors))}")
+
