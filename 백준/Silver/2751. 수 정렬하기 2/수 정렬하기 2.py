980438325db87761cff73b7c2e2ec191b31128cc
Jiwoong Jung <jacejung.dev@gmail.com>
2025-08-28 09:17:23 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 732 ms, Memory: 111636 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -2,7 +2,8 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-num_list = [int(input()) for _ in range(N)]
-num_list.sort()
-for num in num_list:
-    print(num)
+ordered_list = [0] * 2000001
+for _ in range(N):
+    i = int(input())
+    ordered_list[i+ 1000000] = 1
+print("\n".join(str(i) for i in range(-1000000,1000001,1) if ordered_list[i+1000000]))
