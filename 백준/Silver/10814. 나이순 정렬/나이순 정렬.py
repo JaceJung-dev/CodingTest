9389a3f2adef879c6218e7280aea3e0164dbd0a5
Jiwoong Jung <jacejung.dev@gmail.com>
2025-08-29 09:42:30 +0900

    [Silver V] Title: 나이순 정렬, Time: 220 ms, Memory: 46020 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+people = []
+for _ in range(N):
+    age, name = input().split()
+    age = int(age)
+    people.append((age, name))
+    
+people.sort(key=lambda x: x[0])
+
+for person in people:
+    print(person[0], person[1])
+
+
+    
