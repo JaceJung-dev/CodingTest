6756be08f0dee2c96c0b622b15c3c5f3e224783d
정지웅_T8185 <jacejung.dev@gmail.com>
2025-08-31 13:13:56 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.48 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,24 +1,11 @@
-def cvt_binary(num):
-    temp = ""
-    while num > 0:
-        left = num % 2
-        temp += str(left)
-        num = num // 2
-    return temp[::-1]
-
 def solution(s):
     answer = []
     zero_count = 0
     cvt_count = 0
     while s != "1":
-        temp = ""
-        for char in s:
-            if char == "0":
-                zero_count += 1
-                continue
-            else:
-                temp += char
-        s = cvt_binary(len(temp))
         cvt_count += 1
+        one_count = s.count("1")
+        zero_count += len(s) - one_count
+        s = bin(one_count)[2:]
     answer = [cvt_count, zero_count]
     return answer
