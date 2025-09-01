0ea6dc758f5e5c196ab72e9394d071cb34d3c658
정지웅_T8185 <jacejung.dev@gmail.com>
2025-09-02 08:31:31 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+coins = [int(input()) for _ in range(N)]
+coins.sort(reverse=True)
+
+count = 0
+for coin in coins:
+    count += K // coin
+    K %= coin
+    
+    if K == 0:
+        break
+        
+print(count)
