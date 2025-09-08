77de57c56d4651fc05fcfa087bc041ff6331e2f5
정지웅_T8185 <jacejung.dev@gmail.com>
2025-09-08 14:59:05 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.39 ms, Memory: 9.48 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -8,6 +8,9 @@ def solution(maps):
     if maps[0][0] == 0 or maps[h - 1][w - 1] == 0:
         return -1
     
+    # 최단거리를 찾는 case >>> BFS
+    # DFS를 사용하면 한 방향으로 끝까지 탐색하는 방식으로 모든 경로를 확인하고 최솟값을 선택하게 됨
+    # BFS는 진행도중 도착지점에 도착하면 그것이 최단거리
     visited = [[False] * w for _ in range(h)]
     queue = deque([(0, 0, 1)])    # 행, 열, 움직인 횟수
     visited[0][0] = True
@@ -15,17 +18,21 @@ def solution(maps):
     while queue:
         row, col, count = queue.popleft()
         
+        # 도착지점에 도착한 경우
         if row == h - 1 and col == w - 1:
             return count
         
         for dx, dy in directions:
             new_row, new_col = row + dx, col + dy
             
+            # 진영 밖으로 나가는 경우(제외)
             if not (0 <= new_row <= h - 1 and 0 <= new_col <= w - 1):
                 continue
             
+            # 이동한 곳이 처음 가는 곳이며, 벽이 아닌 경우(queue에 넣기)
             if not visited[new_row][new_col] and maps[new_row][new_col] == 1:
                 queue.append((new_row, new_col, count + 1))
                 visited[new_row][new_col] = True
-                
+    
+    # 빈 칸을 모두 돌았지만, 도착지점에 도착 못한 경우
     return -1
