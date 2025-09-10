e846a7950d0d507aeba110511364266a53c7d0fc
정지웅_T8185 <jacejung.dev@gmail.com>
2025-09-10 14:38:31 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,19 +1,23 @@
-from collections import deque
 import math
+from collections import deque
+
 
 def solution(progresses, speeds):
-    answer = []
     
+    answer = []
+    # (남은 일수, 원래 순서)
     queue = deque([(math.ceil((100 - progresses[i]) / speeds[i]) ,i) for i in range(len(progresses))])
 
     while queue:
         cur = queue.popleft()
         count = 1
         
+        # 일괄처리(현재 선택된 작업보다 일찍 끝나는 작업들)된 것들 같이 카운팅
         while queue and queue[0][0] <= cur[0]:
             queue.popleft()
             count += 1
         
+        # 한번 배포될 때 진행되는 작업 개수
         answer.append(count)
                 
     return answer
