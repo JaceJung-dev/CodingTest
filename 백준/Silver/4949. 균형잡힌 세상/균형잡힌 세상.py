1787fe7eb36ea059c2f727a692dfed3c50983e99
정지웅_T8185 <jacejung.dev@gmail.com>
2025-09-21 08:34:08 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -2,30 +2,30 @@ import sys
 
 input = sys.stdin.readline
 
+
+def is_balanced(sentence):
+    stack = []
+
+    for char in sentence:
+        if char in "([":
+            stack.append(char)
+        elif char in ")]":
+            if stack and (
+                (stack[-1] == "(" and char == ")") or (stack[-1] == "[" and char == "]")
+            ):
+                stack.pop()
+            else:
+                return False
+    return not stack
+
+
 while True:
     sentence = input().rstrip()
 
     if sentence == ".":
         break
 
-    stack = []
-
-    for char in sentence:
-
-        if stack:
-            cur = stack.pop()
-
-            if cur == "(" and char == ")":
-                continue
-            elif cur == "[" and char == "]":
-                continue
-            else:
-                stack.append(cur)
-
-        if char in ["(", ")", "[", "]"]:
-            stack.append(char)
-
-    if stack:
-        print("no")
-    else:
+    if is_balanced(sentence):
         print("yes")
+    else:
+        print("no")
