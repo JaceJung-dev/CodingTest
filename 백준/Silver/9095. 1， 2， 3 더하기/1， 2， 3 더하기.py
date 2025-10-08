f7738114ea28b7c1f4f2781f908de52f18d89d79
정지웅_T8185 <jacejung.dev@gmail.com>
2025-10-08 10:36:23 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,24 +1,17 @@
 import sys
-from collections import deque
 
 
 input = sys.stdin.readline
 
 T = int(input())
+dp = [0] * 13
+dp[1] = 1  # 1
+dp[2] = 2  # 1+1, 2
+dp[3] = 4  # 1+1+1, 1+2, 2+1, 3
+
+for i in range(4, 13):
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
 
 for _ in range(T):
     target = int(input())
-    queue = deque([0])
-    count = 0
-
-    while queue:
-        cur = queue.popleft()
-
-        if cur == target:
-            count += 1
-        elif cur < target:
-            queue.append(cur + 1)
-            queue.append(cur + 2)
-            queue.append(cur + 3)
-
-    print(count)
+    print(dp[target])
