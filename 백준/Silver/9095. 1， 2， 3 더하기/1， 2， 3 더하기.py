05cfd44a699ee7d84a6f4c39990b57241a970b36
정지웅_T8185 <jacejung.dev@gmail.com>
2025-10-08 10:15:42 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 56 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -8,17 +8,17 @@ T = int(input())
 
 for _ in range(T):
     target = int(input())
-    fin_combination = set()
-    queue = deque([[1], [2], [3]])
+    queue = deque([0])
+    count = 0
 
     while queue:
         cur = queue.popleft()
 
-        if sum(cur) < target:
-            queue.append(cur + [1])
-            queue.append(cur + [2])
-            queue.append(cur + [3])
-        elif sum(cur) == target:
-            fin_combination.add(tuple(cur))
+        if cur == target:
+            count += 1
+        elif cur < target:
+            queue.append(cur + 1)
+            queue.append(cur + 2)
+            queue.append(cur + 3)
 
-    print(len(fin_combination))
+    print(count)
