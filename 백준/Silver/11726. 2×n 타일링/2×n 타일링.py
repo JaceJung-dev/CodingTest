ef1b2bf586674065f41cc6974b5299b4bf540c58
정지웅_T8185 <jacejung.dev@gmail.com>
2025-10-11 10:06:16 +0900

    [Silver III] Title: 2×n 타일링, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,21 +1,11 @@
 import sys
-from collections import deque
-
 input = sys.stdin.readline
 
-
-def fibo(n):
-    a, b = 1, 2
-
-    if n == 1:
-        return a
-    else:
-        for _ in range(3, n + 1):
-            a, b = b, a + b
-        return b
-
-
 N = int(input())
-ans = fibo(N) % 10007
 
-print(ans)
+dp = [0] * (1001)
+dp[1], dp[2] = 1, 2
+for i in range(3, N + 1):
+    dp[i] = dp[i - 2] + dp[i - 1]
+    
+print(dp[N] % 10007)
