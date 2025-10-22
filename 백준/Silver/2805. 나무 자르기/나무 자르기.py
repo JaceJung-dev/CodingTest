36a95d4a18a0875b631f35e18a8db33ffc23f67e
정지웅_T8185 <jacejung.dev@gmail.com>
2025-10-22 20:05:14 +0900

    [Silver II] Title: 나무 자르기, Time: 2132 ms, Memory: 148224 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -3,13 +3,7 @@ import sys
 input = sys.stdin.readline
 
 def get_tree_length(trees, n):
-    tree_len = 0
-    for tree in trees:
-        cut_len = tree - n
-        if cut_len >= 0:
-            tree_len += cut_len
-
-    return tree_len
+    return sum((tree - n) for tree in trees if tree > n)
 
 N, M = map(int, input().split())
 trees = list(map(int, input().split()))
@@ -19,7 +13,6 @@ final_height = 0
 
 while low <= high:
     middle = (low + high) // 2
-
     tree_len = get_tree_length(trees, middle)
     
     if tree_len >= M:
