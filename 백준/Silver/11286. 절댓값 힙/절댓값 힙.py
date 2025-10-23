78b2a79538e88ae3c4afab8bf4a7519bdce3f283
정지웅_T8185 <jacejung.dev@gmail.com>
2025-10-23 20:06:11 +0900

    [Silver I] Title: 절댓값 힙, Time: 128 ms, Memory: 42144 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -0,0 +1,16 @@
+import heapq, sys
+
+input = sys.stdin.readline
+
+N = int(input())
+heap = []
+for _ in range(N):
+    op = int(input())
+    
+    if op == 0:
+        if heap:
+            print(heapq.heappop(heap)[1])
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, (abs(op), op))
