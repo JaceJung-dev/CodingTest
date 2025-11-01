3afe5174652ea8e4cdd941cff3b773e25fab156e
정지웅_T8185 <jacejung.dev@gmail.com>
2025-11-01 15:26:16 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -5,12 +5,10 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 maze = [input().strip() for _ in range(N)]
-visited = [[False] * M for _ in range(N)]
 dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 dist = [[0] * M for _ in range(N)]
 
 queue = deque([(0, 0)])
-visited[0][0] = True
 dist[0][0] = 1
 
 while queue:
@@ -24,7 +22,6 @@ while queue:
         n_x, n_y = x + dx, y + dy
 
         if 0 <= n_y < N and 0 <= n_x < M:
-            if not visited[n_y][n_x] and maze[n_y][n_x] == "1":
+            if not dist[n_y][n_x] and maze[n_y][n_x] == "1":
                 queue.append((n_y, n_x))
-                visited[n_y][n_x] = True
                 dist[n_y][n_x] = dist[y][x] + 1
