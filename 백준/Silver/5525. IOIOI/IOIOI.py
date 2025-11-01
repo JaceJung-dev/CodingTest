34ce5fbf519811b1948d4ca64295d33b505666ba
정지웅_T8185 <jacejung.dev@gmail.com>
2025-11-02 08:54:37 +0900

    [Silver I] Title: IOIOI, Time: 276 ms, Memory: 34368 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -6,11 +6,22 @@ N = int(input())
 M = int(input())
 S = input().strip()
 
-pn = "IO" * N + "I"
 count = 0
+answer = 0
+i = 0
 
-for i in range(len(S)):
-    if S[i:].startswith(pn):
+while i <= M -3:
+    if S[i] == "I" and S[i + 1] == "O" and S[i + 2] == "I":
         count += 1
+    
+        if count >= N:
+            answer += 1
+            count -= 1
 
-print(count)
+        i += 2
+    else:
+        count = 0
+        i += 1
+
+print(answer)
+
