e679e6d5edd580bb8bf19c1c5243e829238f2d1c
정지웅_T8185 <jacejung.dev@gmail.com>
2025-11-08 22:05:09 +0900

    [Silver I] Title: 경로 찾기, Time: 72 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -12,35 +12,22 @@ for i in range(N):
         if row[j] == 1:
             graph[i].append(j)
 
+edge = [[0] * N for _ in range(N)]
 
-def has_edge(start, end):
+for start in range(N):
     visited = [False] * N
     queue = deque([start])
-    visited[start] = True
 
     while queue:
         cur = queue.popleft()
-
+        
         for next_node in graph[cur]:
-            if next_node == end:
-                return True
-
             if not visited[next_node]:
-                queue.append(next_node)
                 visited[next_node] = True
+                queue.append(next_node)
 
-    return False
-
-
-edge = []
-for i in range(N):
-    row = []
-    for j in range(N):
-        if has_edge(i, j):
-            row.append(1)
-        else:
-            row.append(0)
-    edge.append(row)
+    for end in range(N):
+        edge[start][end] = 1 if visited[end] else 0
 
 for row in edge:
     print(*row)
