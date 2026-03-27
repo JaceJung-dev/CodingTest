3a2f9d7391b1385fe36808d2196b67ed4409dd42
정지웅 <jacejung.dev@gmail.com>
2026-03-27 21:44:26 +0900

    [Bronze I] Title: 암호 키, Time: 328 ms, Memory: 43916 KB -BaekjoonHub

--- 백준/Bronze/1816. 암호 키/암호 키.py
+++ 백준/Bronze/1816. 암호 키/암호 키.py
@@ -2,13 +2,23 @@ import sys
 
 input = sys.stdin.readline
 
-CUTOFF = 10 ** 6
+LIMIT = 10 ** 6
+is_prime = [True] * (LIMIT + 1)
+is_prime[0] = is_prime[1] = False
+
+for i in range(2, int(LIMIT ** 0.5) + 1):
+    if is_prime[i]:
+        for j in range(i * i, LIMIT + 1, i):
+            is_prime[j] = False
+
+primes = [num for num in range(2, LIMIT + 1) if is_prime[num]]
+
 N = int(input())
 for _ in range(N):
     S = int(input())
 
-    for i in range(2, CUTOFF + 1):
-        if S % i == 0:
+    for prime in primes:
+        if S % prime == 0:
             print("NO")
             break
     else:
