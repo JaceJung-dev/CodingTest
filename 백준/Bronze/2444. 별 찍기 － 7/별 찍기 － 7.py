fb4e574af54e2fe95ef2eff1680483a09c74132a
정지웅 <jacejung.dev@gmail.com>
2026-04-05 11:20:02 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+for i in range(1, N + 1):
+    for _ in range(N - i):
+        print(" ", end="")
+    for _ in range(2 * i - 1):
+        print("*", end="")
+    print()
+
+for i in range(N - 1, 0, -1):
+    for _ in range(N - i):
+        print(" ", end="")
+    for _ in range(2 * i - 1):
+        print("*", end="")
+    print()
