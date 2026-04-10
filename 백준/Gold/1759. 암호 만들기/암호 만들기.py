c442b45c1262cc87dd2e1dcf8a4452d07d1d1e43
정지웅 <jacejung.dev@gmail.com>
2026-04-10 15:12:13 +0900

    [Gold V] Title: 암호 만들기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -0,0 +1,36 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def is_possible():
+    global L, C, letters, selections
+
+    v_cnt = 0
+
+    for char in selections:
+        v_cnt += char in vowels
+    c_cnt = L - v_cnt
+
+    return v_cnt >= 1 and c_cnt >= 2
+
+
+def combination(index, level):
+    if level == L:
+        if is_possible():
+            print("".join(selections))
+        return
+
+    for i in range(index, C):
+        selections.append(letters[i])
+        combination(i + 1, level + 1)
+        selections.pop()
+
+
+L, C = map(int, input().split())
+letters = input().split()
+letters.sort()
+vowels = "aeiou"
+
+selections = []
+combination(0, 0)
