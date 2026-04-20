9e787cc1014c85b928a8addd30cc6fa8ffd78944
정지웅 <jacejung.dev@gmail.com>
2026-04-20 17:35:41 +0900

    [Silver I] Title: 숨바꼭질, Time: 132 ms, Memory: 36656 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -3,32 +3,23 @@ from collections import deque
 
 input = sys.stdin.readline
 
-N, K = map(int, input().split())
-visited = [False] * 100001
-dist = [0] * 100001
+MAX = 10**5
+N, M = map(int, input().split())
+visited = [False] * (MAX + 1)
 
 
-queue = deque([N])
+queue = deque()
+queue.append((0, N))
 visited[N] = True
 
 while queue:
-    cur = queue.popleft()
-    
-    if cur == K:
-        break
-    
-    for i in range(3):
-        if i % 3 == 0:
-            new_pos = cur + 1
-        elif i % 3 == 1:
-            new_pos = cur - 1
-        else:
-            new_pos = cur * 2
-        
-        if 0 <= new_pos <= 100000:
-            if not visited[new_pos]:
-                queue.append(new_pos)
-                visited[new_pos] = True
-                dist[new_pos] = dist[cur] + 1
+    time, pos = queue.popleft()
 
-print(dist[K])
+    if pos == M:
+        print(time)
+        exit()
+
+    for nxt_pos in [pos - 1, pos + 1, pos * 2]:
+        if (0 <= nxt_pos <= MAX) and (not visited[nxt_pos]):
+            queue.append((time + 1, nxt_pos))
+            visited[nxt_pos] = True
